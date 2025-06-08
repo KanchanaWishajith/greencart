b304c3ff588f91796b095240aed3a25a14eebfad
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-06-08 20:52:57 +0530

    fix errors

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,3 +1,29 @@
-import { createContext } from "react";
+import React, { createContext, useState, useContext } from "react";
 
-export const AppContext = React.createContext();
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const navigate = useNavigate();
+
+  const value = {
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
