40014fd4ea71ee5227ad55dce07a1e8062dfd905
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-06-08 23:24:49 +0530

    Create Categories

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
